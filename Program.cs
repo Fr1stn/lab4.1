bbcf6067afde96729129558f719a248237be4b8d
Fr1stn <happipampers00@gmail.com>
2025-09-19 00:28:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+Console.WriteLine("Сколько чисел вы хотите ввести?");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите числа:");
+int nums = 0;
+int i = 0;
+while (i < n)
+{
+    i++;
+    if (Convert.ToInt16(Console.ReadLine()) % 2 == 1)
+    {
+        nums++;
+    }
+}
+Console.WriteLine($"Всего нечетных чисел: {nums}");
